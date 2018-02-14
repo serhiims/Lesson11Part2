448d535b79fbc18a56c530299ae689ce871daf76
serhii.malenko <malenko@renatus.com>
2018-02-14 22:12:51 +0200

    Lesson #11 Part #2. Destroy entity fix.

--- Assets/Scripts/Systems/DestroyedSystem.cs
+++ Assets/Scripts/Systems/DestroyedSystem.cs
@@ -6,50 +6,53 @@ using UnityEngine;
 
 public class DestroyedSystem : ReactiveSystem<GameEntity>
 {
-	public DestroyedSystem (Contexts contexts) : base (contexts.game)
-	{
-	}
+    public DestroyedSystem (Contexts contexts) : base (contexts.game)
+    {
+    }
 
-	protected override ICollector<GameEntity> GetTrigger (IContext<GameEntity> context)
-	{
-		return context.CreateCollector (GameMatcher.Destroyed);
-	}
+    protected override ICollector<GameEntity> GetTrigger (IContext<GameEntity> context)
+    {
+        return context.CreateCollector (GameMatcher.Destroyed);
+    }
 
-	protected override bool Filter (GameEntity entity)
-	{
-		return entity.hasDestroyed;
-	}
+    protected override bool Filter (GameEntity entity)
+    {
+        return entity.hasDestroyed;
+    }
 
-	protected override void Execute (List<GameEntity> entities)
-	{
-        
-		foreach (var entity in entities) {
-			if (entity.hasView && entity.hasPosition && entity.hasDestroyed && entity.destroyed.value) {
-				entity.RemovePosition ();
-				if (entity.hasView) {
-					DestroyView (entity.view);
-				}
-			}
-		}
-	}
+    protected override void Execute (List<GameEntity> entities)
+    {        
+        foreach (var entity in entities) {
+            if (entity.hasView && entity.hasPosition && entity.hasDestroyed && entity.destroyed.value) {
+                entity.RemovePosition ();
+                DestroyEntity (entity);
+            }
+        }
+    }
 
-	private void DestroyView (ViewComponent view)
-	{
-		var spriteRenderer = view.value.GetComponent<SpriteRenderer> ();
-		spriteRenderer.transform.DetachChildren ();
+    private void DestroyEntity (GameEntity entity)
+    {
+        ViewComponent view = entity.view;
+        if(view == null)
+        {
+            entity.Destroy();
+            return;
+        }
+        var spriteRenderer = view.value.GetComponent<SpriteRenderer> ();
+        spriteRenderer.transform.DetachChildren ();
 
-		spriteRenderer.sortingOrder = 2;
-		var collinder2D = view.value.GetComponent<Collider2D> ();
-		if (collinder2D != null) {
-			collinder2D.enabled = false;
-		}
-		view.value.gameObject.Unlink ();
-		Object.Destroy (view.value.gameObject);
-//        view.value.gameObject.transform
-//                 .DOScale(Vector3.one * 1.5f, 0.5f)
-//                 .OnComplete(() => {
-//                     view.value.gameObject.Unlink();
-//                     Object.Destroy(view.value.gameObject);
-//                 });    
-	}
+        spriteRenderer.sortingOrder = 2;
+        var collinder2D = view.value.GetComponent<Collider2D> ();
+        if (collinder2D != null) {
+            collinder2D.enabled = false;
+        }        
+        spriteRenderer.DOFade(0, 0.45f);
+        view.value.gameObject.transform
+                .DOScale(Vector3.one * 1.5f, 0.5f)
+                .OnComplete(() => {
+                    view.value.gameObject.Unlink();
+                    Object.Destroy(view.value.gameObject);
+                    entity.Destroy();
+                });    
+    }
 }
